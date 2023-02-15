2dc5a386d5a6fef8ab82019308bda45c4d462473
Gideon de Swardt <gideon@fusedigital.io>
2023-02-15 15:17:43 +0000

    Implement plugable commands for app service
    Used reflection to automatically discover commands and options that
    inherit from the IPdfCommandAsync and IPdfCommandOptions interfaces.
    
    Updated the quick setup application service to also handle all the
    commands and argument parser errors.

---
+++ src/FuseDigital.PdfSnip.Application/PdfCommandOptions.cs
@@ -0,0 +1,15 @@
+using CommandLine;
+using Microsoft.Extensions.Logging;
+
+namespace FuseDigital.PdfSnip;
+
+public abstract class PdfCommandOptions : IPdfCommandOptions
+{
+    [Option(longName: "verbosity", shortName: 'v', Default = LogLevel.Warning,
+        HelpText = "Set the verbosity level. Allowed values are Trace, Debug, Information and Warning")]
+    public LogLevel Verbosity { get; set; }
+
+    public abstract Type GetCommandType();
+
+    protected const string ApplicationAlias = "pdfsnip";
+}
--- src/FuseDigital.PdfSnip.Application/PdfSnipApplicationModule.cs
+++ src/FuseDigital.PdfSnip.Application/PdfSnipApplicationModule.cs
@@ -1,9 +1,5 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Application;
-using Volo.Abp.Application.Services;
 using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
 
@@ -16,26 +12,12 @@ namespace FuseDigital.PdfSnip;
 ]
 public class PdfSnipApplicationModule : AbpModule
 {
-}
-
-public class PdfSnipApplicationService : ApplicationService
-{
-    private PdfSnipOptions Settings { get; }
-
-    public PdfSnipApplicationService(IOptions<PdfSnipOptions> options)
+    public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        Settings = options.Value;
-    }
-
-    public async Task RunAsync(IEnumerable<string> args)
-    {
-        Logger.LogInformation("Pdf Snipping Tool ({ProductVersion}) (https://github.com/fuse-digital/pdf-snip)", GetProductVersion());
-    }
-    
-    private string GetProductVersion()
-    {
-        var assembly = Assembly.GetEntryAssembly();
-        var version = FileVersionInfo.GetVersionInfo(assembly.Location);
-        return version.FileVersion;
+        context.Services.AddAutoMapperObjectMapper<PdfSnipApplicationModule>();
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<PdfSnipApplicationModule>(validate: true);
+        });
     }
 }
